124bbe62fc2f53993cb17e245c9fa6b139824f7f
Baha1440 <nbaha1080@gmail.com>
2023-11-18 11:42:09 +0100

    updated the frontend url

--- server/server.js
+++ server/server.js
@@ -19,13 +19,13 @@ connectDB();
 app.use(express.json());
 
 const corsOption = {
-  origin: "https://contact-frontend-gub8.onrender.com",
+  origin: "https://mern-contact-frontend-asbs.onrender.com",
 };
 app.use(cors(corsOption));
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://contact-frontend-gub8.onrender.com"
+    "https://mern-contact-frontend-asbs.onrender.com"
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
